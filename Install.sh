d182883ba49ed191d900c3c09f421780eaa5ee58
dewaade11 <83090110+dewaade11@users.noreply.github.com>
2024-12-12 22:28:16 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,12 +5,23 @@ echo "Memperbarui sistem dan menginstal curl..."
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl
 
-# Menginstal nvm dan PM2
-echo "Menginstal nvm dan PM2..."
+# Menginstal nvm
+echo "Menginstal nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source ~/.bashrc
-nvm list-remote
-nvm install v20.13.0 && npm install pm2 -g
+
+# Memuat nvm ke dalam lingkungan
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \ . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \ . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Memeriksa versi Node.js yang tersedia dan menginstal Node.js serta npm
+echo "Menginstal Node.js dan npm..."
+nvm install v20.13.0
+nvm use v20.13.0
+
+# Menginstal PM2
+echo "Menginstal PM2..."
+npm install pm2 -g
 
 # Menginstal Docker
 echo "Menginstal Docker..."
@@ -27,8 +38,8 @@ docker pull jc21/nginx-proxy-manager:latest
 # Menjalankan container Docker untuk Nginx Proxy Manager
 echo "Menjalankan container Docker Nginx Proxy Manager..."
 docker run -d --name=nginxproxymanager \
- -v ./data:/data \
- -v ./letsencrypt:/etc/letsencrypt \
+ -v $PWD/data:/data \
+ -v $PWD/letsencrypt:/etc/letsencrypt \
  --network host \
  --restart unless-stopped \
  jc21/nginx-proxy-manager
